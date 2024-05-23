6ebcfd27cd79ab482d3e272c66bfaffaa8885190
CVTenorio <202310471@fit.edu.ph>
2024-05-24 03:06:54 +0800

    still modifying/and testing the home page (welcome page)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 let containersReleased = false;
 
 function releaseContainers () {
-    const container2 = document.querySelector('.HomeContainerOne');
+    const container1 = document.querySelector('.HomeContainerOne');
     const container3 = document.querySelector('.HomeContainerThree');
 
     if (containersReleased) {
-        container2.classList.remove('released');
+        container1.classList.remove('released');
         container3.classList.remove('released');
-        container2.classList.add('hidden');
+        container1.classList.add('hidden');
         container3.classList.add('hidden');
     }else {
-        container2.classList.remove('hidden');
+        container1.classList.remove('hidden');
         container3.classList.remove('hidden');
-        container2.classList.add('released');
+        container1.classList.add('released');
         container3.classList.add('released');
     }
 
